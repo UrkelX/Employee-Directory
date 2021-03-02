e10570b53ee0bb7c6262ce9d0e7e42df9e1fb274
UrkelX <jordon@blackbirdrevolt.com>
2021-03-02 17:18:52 -0600

    Testing card

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,20 +1,3 @@
 import React from "react";
-// import API from "../utils/API";
+import API from "../utils/API";
 
- function EmployeeDeck({ name, picture, login, email, phone}) { 
-    return (
-        <div>
-            <img
-                style={{ maxWidth: "60px"}}
-                src={picture}
-                alt="employee"
-            />
-            <p>{name}</p>
-            <p>{login}</p>
-            <p>{email}</p>
-            <p>{phone}</p>
-        </div>
-    )
-}
-
-export default EmployeeDeck;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
+// Export an object containing methods we'll use for accessing the random user API
 export default {
-  search: function () {
-    return axios.get("https://randomuser.me/api/?results=20");
+  fetchEmployees: function() {
+    return axios
+      .get("https://randomuser.me/api/?results=20")
+      .then(res => {
+        const users = res.data;
+        return users.map(user => {
+          return {
+            login: user.login,
+            image: user.avatar_url,
+            profileUrl: user.html_url
+          };
+        });
+      });
   }
 };
