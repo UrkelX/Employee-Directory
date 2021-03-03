28a757b87f8eeb5e864e596608848dbaa7213935
UrkelX <jordon@blackbirdrevolt.com>
2021-03-03 01:12:59 -0600

    Image edits

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -23,12 +23,12 @@ function Card({ title, image, profileUrl, email, handleBtnClick }) {
         data-value="next"
       />
 
-      <img
+      {/* <img
         style={{ maxWidth: "60px" }}
         className="rounded-full shadow-md border border-gray-300"
         src={image}
         alt="employee"
-      />
+      /> */}
       <p className="font-bold text-md">{title}</p>
       <p>Content Here</p>
       <p className="text-blue-500">{email}</p>
