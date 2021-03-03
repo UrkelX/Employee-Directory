b35d8561e642077de51d025e219ab0535268091c
UrkelX <jordon@blackbirdrevolt.com>
2021-03-03 01:11:20 -0600

    Image edits

--- src/components/CardImage/index.js
+++ src/components/CardImage/index.js
@@ -3,7 +3,12 @@ import React from "react";
 function CardImg({ image }) {
   return (
     <div>
-      <img className="card-img" src={image} alt="user thumbnail" />
+      <img
+        style={{ maxWidth: "60px" }}
+        className="rounded-full shadow-md border border-gray-300"
+        src={image}
+        alt="employee"
+      />
       {!image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
     </div>
   );
