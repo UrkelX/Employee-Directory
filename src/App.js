8bbb900d68d4ad9b4a00f2215aee2208590fdb6f
UrkelX <jordon@blackbirdrevolt.com>
2021-03-03 17:05:08 -0600

    updated API

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div> "Hello"
+    <div>
       <Navbar />
       <Wrapper>
         <Gallery />
--- src/utils/API.js
+++ src/utils/API.js
@@ -5,17 +5,19 @@ export default {
   fetchEmployees: function() {
     return axios
       .get("https://randomuser.me/api/?results=20")
-      .then(res => {
-        const employees = res.data.results;
-        return employees.map(employee => {
-          return {
-            id: employee.id.value,
-            firstName: employee.name.first,
-            lastName: employee.name.last,
-            email: employee.email,
-            picture: employee.picture.thumbnail
-          };
-        });
-      });
+      
   }
 };
+
+// .then(res => {
+//     const employees = res.data.results;
+//     return employees.map(employee => {
+//       return {
+//         id: employee.id.value,
+//         firstName: employee.name.first,
+//         lastName: employee.name.last,
+//         email: employee.email,
+//         picture: employee.picture.thumbnail
+//       };
+//     });
+//   });
