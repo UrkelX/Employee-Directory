564dc7517ba733989b217a6c0dc2eeda4bb8ecbe
UrkelX <jordon@blackbirdrevolt.com>
2021-03-03 19:47:05 -0600

    Tired of this assignment

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./style.css";
 
 
-function Card(props) {
+function Card(firstName, lastName, email, phone, picture) {
   return (
 
       <div className="md:flex bg-white shadow text-gray-800 my-4 py-4 px-10 rounded-md items-center justify-between hover:bg-gray-300">
-          <div><img alt={props.last} src={props.image}></img></div>
-          <div>{props.title} {props.first} {props.last} </div>
-          <div>{props.gender}</div>
-          <div>{props.age}</div>
-          <div>{props.phone}</div>
-          <div>{props.email}</div>
+          <div><img alt={picture} src={picture}></img></div>
+          <div>{firstName} {lastName} </div>
+          <div>{phone}</div>
+          <div>{email}</div>
       </div>
   );
 }
