c8aace723ef870298920fd70655b9a418dc8acbb
UrkelX <jordon@blackbirdrevolt.com>
2021-03-04 16:34:08 -0600

    Updated navbar and footer

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,26 +1,13 @@
 import React from "react";
-import CardBody from "../CardBody";
-import CardBtn from "../CardBtn";
-import CardImg from "../CardImage";
-import CardHeading from "../CardHeading";
 import "./style.css";
 
-function Card({ firstName, picture, phone, email, handleBtnClick }) {
+function Card({ firstName, picture, phone, email }) {
   return (
     <div>
-      <CardHeading firstName={firstName} />
-      <CardImg picture={picture} />
-      <CardBody phone={phone} email={email}/>
-      <CardBtn
-        style={{ opacity: picture ? 1 : 0 }}
-        onClick={handleBtnClick}
-        data-value="back"
-      />
-      <CardBtn
-        style={{ opacity: picture ? 1 : 0 }}
-        onClick={handleBtnClick}
-        data-value="next"
-      />
+      {firstName}
+      {picture}
+      {phone}
+      {email}
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" href="https://urkelx.github.io/employee-directory/">
         Employee Directory
       </a>
     </nav>
