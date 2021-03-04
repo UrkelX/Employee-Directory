1ad8eb94b0cbf29e587e7d36f15b9b9d690f9267
UrkelX <jordon@blackbirdrevolt.com>
2021-03-03 19:51:24 -0600

    Forgotten bracket

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 
-function Card(firstName, lastName, email, phone, picture) {
+function Card({firstName, lastName, email, phone, picture}) {
   return (
 
       <div className="md:flex bg-white shadow text-gray-800 my-4 py-4 px-10 rounded-md items-center justify-between hover:bg-gray-300">
