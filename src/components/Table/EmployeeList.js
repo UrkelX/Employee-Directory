9fb8f178768d741a7f5aa669fcd719a6bae1699c
UrkelX <jordon@blackbirdrevolt.com>
2021-03-03 20:44:41 -0600

    List updated

--- src/components/Table/EmployeeList.js
+++ src/components/Table/EmployeeList.js
@@ -1,1 +1,18 @@
+import React from "react";
+import Card from "./Card";
 
+
+function EmployeeList ({ data }) { 
+    return (
+        data.map(employee => (
+            <Card
+                image={employee.picture}
+                name={employee.name}
+                email={employee.email}
+                phone={employee.phone}
+            />
+            ))
+    )
+}
+
+export default EmployeeList;
