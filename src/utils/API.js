258f96ec9c259dc87c9bf8fe76deb3a02ca057a5
UrkelX <jordon@blackbirdrevolt.com>
2021-03-03 21:23:21 -0600

    Edit API

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the random user API
 export default {
-  fetchEmployees: function() {
-    return axios
-      .get("https://randomuser.me/api/?results=20")
-  }
+  fetchEmployees: function () {
+    return axios.get("https://randomuser.me/api/?results=20").then((res) => {
+      const users = res.data;
+      return users.map((employee) => {
+        return {
+          firstName: employee.name.first,
+          picture: employee.picture,
+          phone: employee.phone,
+          email: employee.email,
+        };
+      });
+    });
+  },
 };
-
